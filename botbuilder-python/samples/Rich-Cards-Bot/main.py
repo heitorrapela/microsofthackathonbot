3d1771ef9b5a8a247daa94c9ce386fbfe73aa55d
heitorrapela <hrm@cin.ufpe.br>
2019-03-21 10:27:53 -0300

    Update messages

--- botbuilder-python/samples/Rich-Cards-Bot/main.py
+++ botbuilder-python/samples/Rich-Cards-Bot/main.py
@@ -153,30 +153,16 @@ async def handle_message(context: TurnContext) -> web.Response:
             return await card_response(context)
         else:
             state.in_prompt = True
-            prompt_message = await create_reply_activity(context.activity, 'Which card would you like to see?\n'
-                                                                           '(1) Adaptive Card\n'
-                                                                           '(2) Animation Card\n'
-                                                                           '(3) Audio Card\n'
-                                                                           '(4) Hero Card\n'
-                                                                           '(5) Receipt Card\n'
-                                                                           '(6) Signin Card\n'
-                                                                           '(7) Thumbnail Card\n'
-                                                                           '(8) Video Card\n'
-                                                                           '(9) All Cards')
+            prompt_message = await create_reply_activity(context.activity, 'Hello, what do you want to do?\n'
+                                                                           '(1) Check Image\n'
+                                                                           '(2) Check Environment\n')
             await context.send_activity(prompt_message)
             return web.Response(status=202)
     else:
         state.in_prompt = True
-        prompt_message = await create_reply_activity(context.activity, 'Which card would you like to see?\n'
-                                                                       '(1) Adaptive Card\n'
-                                                                       '(2) Animation Card\n'
-                                                                       '(3) Audio Card\n'
-                                                                       '(4) Hero Card\n'
-                                                                       '(5) Receipt Card\n'
-                                                                       '(6) Signin Card\n'
-                                                                       '(7) Thumbnail Card\n'
-                                                                       '(8) Video Card\n'
-                                                                       '(9) All Cards')
+        prompt_message = await create_reply_activity(context.activity, 'Hello, what do you want to do?\n'
+                                                                       '(1) Check Image\n'
+                                                                       '(2) Check Environment\n')
         await context.send_activity(prompt_message)
         return web.Response(status=202)
 
@@ -184,18 +170,8 @@ async def handle_message(context: TurnContext) -> web.Response:
 async def card_response(context: TurnContext) -> web.Response:
     response = context.activity.text.strip()
     choice_dict = {
-        '1': [create_adaptive_card], 'adaptive card': [create_adaptive_card],
-        '2': [create_animation_card], 'animation card': [create_animation_card],
-        '3': [create_audio_card], 'audio card': [create_audio_card],
-        '4': [create_hero_card], 'hero card': [create_hero_card],
-        '5': [create_receipt_card], 'receipt card': [create_receipt_card],
-        '6': [create_signin_card], 'signin card': [create_signin_card],
-        '7': [create_thumbnail_card], 'thumbnail card': [create_thumbnail_card],
-        '8': [create_video_card], 'video card': [create_video_card],
-        '9': [create_adaptive_card, create_animation_card, create_audio_card, create_hero_card,
-              create_receipt_card, create_signin_card, create_thumbnail_card, create_video_card],
-        'all cards': [create_adaptive_card, create_animation_card, create_audio_card, create_hero_card,
-                      create_receipt_card, create_signin_card, create_thumbnail_card, create_video_card]
+        '1': [create_adaptive_card], 'check image': [create_adaptive_card],
+        '2': [create_animation_card], 'monitor environment': [create_animation_card],
     }
 
     # Get the functions that will generate the card(s) for our response
