ae45805d78b5ad14039bf771c63d01f0b2cbb12e
heitorrapela <hrm@cin.ufpe.br>
2019-03-21 09:37:08 -0300

    add version with pip install

---
+++ pip_install.sh
@@ -0,0 +1,5 @@
+/home/$USER/anaconda2/envs/mshack/bin/pip install -e ./libraries/botbuilder-schema
+/home/$USER/anaconda2/envs/mshack/bin/pip install -e ./libraries/botframework-connector
+/home/$USER/anaconda2/envs/mshack/bin/pip install -e ./libraries/botbuilder-core
+/home/$USER/anaconda2/envs/mshack/bin/pip install -e ./libraries/botbuilder-azure
+/home/$USER/anaconda2/envs/mshack/bin/pip install -r ./libraries/botframework-connector/tests/requirements.txt
